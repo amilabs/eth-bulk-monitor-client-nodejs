1ce669cfac81935444f71a95e86a46b56e30ce01
Artem <artemk@211.ru>
2020-06-22 19:52:56 +0700

    Token data retrieval

--- MonitorClient.js
+++ MonitorClient.js
@@ -6,10 +6,18 @@ let credentials = {};
 
 // Known networks
 let networks = {
-    mainnet: 'https://api-mon.ethplorer.io',
-    kovan: 'https://kovan-api-mon.ethplorer.io',
-    custom: ''
+    mainnet: {
+        api: 'https://api.ethplorer.io',
+        monitor: 'https://api-mon.ethplorer.io'
+    },
+    kovan: {
+        api: 'https://kovan-api.ethplorer.io',
+        monitor: 'https://kovan-api-mon.ethplorer.io'
+    },
+    custom: false
 };
+
+const tokensCache = {};
 
 class MonitorClient extends EventEmitter {
     /**
@@ -37,12 +45,14 @@ class MonitorClient extends EventEmitter {
             if (options.network && (networks[options.network])) {
                 this.options.network = options.network;
                 if (this.options.network === 'custom') {
-                    if (!options.uri) {
+                    if (!options.monitor) {
                         throw new Error('Custom network requires network API uri to be set in options');
                     }
-                    this.uri = options.uri;
+                    this.api = options.api;
+                    this.monitor = options.monitor;
                 } else {
-                    this.uri = networks[options.network];
+                    this.api = networks[options.network].api;
+                    this.monitor = networks[options.network].monitor;
                 }
             } else {
                 throw new Error(`Unknown network ${options.network}`);
@@ -83,6 +93,7 @@ class MonitorClient extends EventEmitter {
      * @returns {undefined}
      */
     watch() {
+        setImmediate(this.intervalHandler(this));
         this._iId = setInterval(this.intervalHandler(this), this.options.interval * 1000);
     }
 
@@ -102,32 +113,66 @@ class MonitorClient extends EventEmitter {
      */
     intervalHandler(_this) {
         return async () => {
-            const transactionsData = await _this.requestUpdates('getPoolLastTransactions');
-            const operationsData = await _this.requestUpdates('getPoolLastOperations');
+            const transactionsData = await _this.getUpdates('getPoolLastTransactions');
+            const operationsData = await _this.getUpdates('getPoolLastOperations');
             if (transactionsData) {
                 for (let address in transactionsData) {
-                    this.emit('data', { address, data: transactionsData, type: 'transaction' });
+                    const data = transactionsData[address];
+                    for (let i = 0; i < data.length; i++) {
+                        this.emit('data', { address, data: data[i], type: 'transaction' });
+                    }
                 }
             }
             if (operationsData) {
                 for (let address in operationsData) {
-                    this.emit('data', { address, data: operationsData, type: 'operation' });
+                    const data = operationsData[address];
+                    for (let i = 0; i < data.length; i++) {
+                        const token = await _this.getToken(data[i].contract);
+                        data[i].token = token;
+                        this.emit('data', { address, data: data[i], type: 'operation' });
+                    }
                 }
             }
         };
+    }
+
+    async getToken(address) {
+        if (tokensCache[address] === undefined) {
+            try {
+                let result = false;
+                const requestUrl = `${this.api}/getTokenInfo/${address.toString().toLowerCase()}?apiKey=${credentials.apiKey}`;
+                const data = await got(requestUrl);
+                if (data && data.body) {
+                    result = JSON.parse(data.body);
+                    JSON.stringify(result);
+                }
+                tokensCache[address] = result;
+            } catch (e) {
+                tokensCache[address] = false;
+                /*
+                this.errors++;
+                if (this.errors >= this.options.maxErrorCount) {
+                    this.errors = 0;
+                    clearInterval(this._iId);
+                    this.emit('unwatched', e.message);
+                }
+                */
+            }
+        }
+        return tokensCache[address];
     }
 
     /**
      *
      * @returns {undefined}
      */
-    async requestUpdates(method) {
+    async getUpdates(method) {
         let result = null;
         if (['getPoolLastTransactions', 'getPoolLastOperations'].indexOf(method) < 0) {
             throw new Error(`Unknown API method ${method}`);
         }
         try {
-            const requestUrl = `${this.uri}/${method}/${credentials.poolId}?apiKey=${credentials.apiKey}&period=${this.options.period}`;
+            const requestUrl = `${this.monitor}/${method}/${credentials.poolId}?apiKey=${credentials.apiKey}&period=${this.options.period}`;
             const data = await got(requestUrl);
             if (data && data.body) {
                 result = JSON.parse(data.body);

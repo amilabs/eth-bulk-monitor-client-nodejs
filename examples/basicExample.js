2f19a74a397b82876ee1b8ec8aa32be113b056ff
Artem <artemk@211.ru>
2020-09-23 19:35:32 +0700

    Tabs removed, dockblock updated

--- examples/basicExample.js
+++ examples/basicExample.js
@@ -11,14 +11,14 @@ const { monitorApp } = require('../index');
 const app = new monitorApp('put your API key here');
 
 /**
- * Watch for the address new transactions/operations and print out any update
+ * Watch for the addresses new transactions/operations and print out any update
  */
 app.watch([
-        '0x0000000000000000000000000000000000000001',
-        '0x0000000000000000000000000000000000000002',
-        '0x0000000000000000000000000000000000000003'
-    ],
-    // Callback for every new transaction or operation
-    (data) => {
-        console.log(data);
-    });
+    '0x0000000000000000000000000000000000000001',
+    '0x0000000000000000000000000000000000000002',
+    '0x0000000000000000000000000000000000000003'
+],
+// Callback for every new transaction or operation
+(data) => {
+    console.log(data);
+});
